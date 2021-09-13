33aa6ce0e8dbf85b55dcf1c6866a89e2ee595071
pjd-code <preston.dihle@gmail.com>
2021-09-12 22:31:27 -0400

    Update install_qiime2.sh

--- install_qiime2.sh
+++ install_qiime2.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-
+#
 #download and install miniconda
 wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh &> Logfile.txt
 chmod +x Miniconda3-4.5.4-Linux-x86_64.sh &>> Logfile.txt
 bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local &>> Logfile.txt
 echo "1. Conda Installed!"
-
+#
 #update conda while keeping python 3.6
 conda install --channel defaults conda python=3.6 --yes &>> Logfile.txt
 conda update --channel defaults --all --yes &>> Logfile.txt
 echo "2. Conda Updated!"
-
+#
 #The instructions for this step come from https://docs.qiime2.org/2021.4/install/native/
 wget https://data.qiime2.org/distro/core/qiime2-2021.4-py38-linux-conda.yml &>> Logfile.txt
 conda env create -n qiime2-2021.4 --file qiime2-2021.4-py38-linux-conda.yml &>> Logfile.txt
-
+#
 #initiate Conda
 conda init &>> Logfile.txt
 echo "3. Conda Initiated!"
